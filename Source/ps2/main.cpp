36859a20ace03a13860886c48226a585f747efd1
Edwin Rijkee <edwin@virtualparadise.org>
2021-12-27 01:28:38 +0100

    Replace clear by slow fade out

--- Source/ps2/main.cpp
+++ Source/ps2/main.cpp
@@ -113,6 +113,58 @@ struct FlurryRenderData {
     }
 };
 
+static qword_t* darkenBackground(qword_t* q) {
+    const float alpha = 0.083333333335f;
+    // VECTOR colors[] = {{1.0f, 0.0f, 0.0f, alpha},
+    //                    {0.0f, 1.0f, 0.0f, alpha},
+    //                    {0.0f, 0.0f, 1.0f, alpha},
+    //                    {0.0f, 1.0f, 1.0f, alpha}};
+    VECTOR colors[] = {{0.0f, 0.0f, 0.0f, alpha},
+                       {0.0f, 0.0f, 0.0f, alpha},
+                       {0.0f, 0.0f, 0.0f, alpha},
+                       {0.0f, 0.0f, 0.0f, alpha}};
+    VECTOR positions[] = {{0, 0, 0, 1},
+                          {2, 0, 0, 1},
+                          {2, 2, 0, 1},
+                          {0, 0, 0, 1}};
+    xyz_t convertedPositions[4];
+    color_t convertedColors[4];
+
+    for (int i=0; i<3; ++i) {
+        convertedPositions[i] = convertXyz(positions[i]);
+    }
+    draw_convert_rgbaq(convertedColors, 3, (vertex_f_t*)positions, (color_f_t*)colors);
+
+    blend_t blend;
+    blend.color1 = BLEND_COLOR_SOURCE;
+    blend.color2 = BLEND_COLOR_DEST;
+    blend.alpha  = BLEND_ALPHA_SOURCE;
+    blend.color3 = BLEND_COLOR_DEST;
+    blend.fixed_alpha = 0x80;
+    q = draw_alpha_blending(q, 0, &blend);
+
+    prim_t prim;
+    prim.type = PRIM_TRIANGLE_FAN;
+    prim.shading = PRIM_SHADE_GOURAUD;
+    prim.mapping = DRAW_DISABLE;
+    prim.fogging = DRAW_DISABLE;
+    prim.blending = DRAW_ENABLE;
+    prim.antialiasing = DRAW_DISABLE;
+    prim.mapping_type = PRIM_MAP_ST;
+    prim.colorfix = PRIM_UNFIXED;
+
+    color_t color = {0};
+    q = draw_prim_start(q, 0, &prim, &color);
+    for (int i=0; i<4; ++i) {
+        q->dw[0] = convertedColors[i].rgbaq;
+        q->dw[1] = convertedPositions[i].xyz;
+        q++;
+    }
+    q = draw_prim_end(q, 2, DRAW_RGBAQ_REGLIST);
+
+    return q;
+}
+
 int renderLoop(global_info_t& flurryInfo, FlurryRenderData& renderData) {
     // glOrtho(0, screenWidth, 0, screenHeight, 0.0, 1.0);
     float left = 0, right = screenWidth, bottom = 0, top = screenHeight,
@@ -145,9 +197,7 @@ int renderLoop(global_info_t& flurryInfo, FlurryRenderData& renderData) {
         qword_t* q = dmatag;
         q++;
 
-        q = draw_clear(q, 0, 2048.0f - screenWidth / 2,
-                       2048.0f - screenHeight / 2, screenWidth, screenHeight,
-                       0x00, 0x00, 0x00);
+        q = darkenBackground(q);
         q = renderData.render(q);
         q = draw_finish(q);
 
